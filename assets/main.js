df0281c5bc4ee47bde44b341e73f78694af07f58
Anton Gara <AntonGara@iMac-Anton-Gara.local>
2019-11-13 13:19:44 +0200

    file update

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var items = document.querySelectorAll('ul li')
+
+    for (var index = 0; index < items.length; index++) {
+        var item = items[index];
+        
+        item.addEventListener('click', function() {
+            var list = this.querySelector('ul')
+            
+            if(list) {
+                list.classList.toggle('is-visible')
+            }
+        })
+    }
+
+    document.addEventListener('click', event => {
+        let target = event.path ? event.path[0] : event.target;
+
+        if(target.origin && target.origin !== window.location.origin) {
+            target.setAttribute('target', '_blank');
+            return;
+        }
+    })
+    
+}
